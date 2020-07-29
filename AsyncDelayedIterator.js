77cb1ea21a61583c280189088a31e1f18bcfac69
Anwesh43 <anweshthecool0@gmail.com>
2020-07-29 20:27:01 +0530

    adding AsyncIterator

--- AsyncDelayedIterator.js
+++ AsyncDelayedIterator.js
@@ -1,5 +1,20 @@
 const delayPromise = (delay) => {
     return new Promise((resolve, reject) => {
-        setTimeout(resolve, delay)
+        setTimeout(() => {
+            resolve()
+        }, delay)
     })
+}
+
+async function* asyncCounter(n) {
+    for (let i = 1; i <= n; i++) {
+        await delayPromise(1000)
+        yield i
+    }
+}
+
+const asyncIterator = async (n) => {
+    for await(let i of asyncCounter(n)) {
+        console.log(i)
+    }
 }
